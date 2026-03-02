4c0935d02bf213f03a906536db2dbeda00825c3e
idoneve <39467668+idoneve@users.noreply.github.com>
2026-03-01 17:31:14 -0700

    Update plot_primes.py

--- plot_primes.py
+++ plot_primes.py
@@ -7,6 +7,57 @@ import subprocess
 import sys
 
 
+def compile_c_program():
+    print("Compiling C program...")
+    result = subprocess.run(["make", "parallel_build"], capture_output=True, text=True)
+    if result.returncode != 0:
+        print("Failed to compile C with make parallel_build")
+        return False
+    print("C program compliled succesfully...\n")
+    return True
+
+
+def run_c_program(processes, threads, iterations, start_num, benchmark_toggle=False):
+    if benchmark_toggle:
+        print(
+            f"Running benchmark on prime finder with {processes} processes, {threads} threads, range {iterations}, start {start_num}..."
+        )
+        result = subprocess.run(
+            [
+                "make",
+                "benchmark",
+                f"ARGS={processes} {threads} {iterations} {start_num}",
+            ],
+            cwd=os.getcwd(),
+            capture_output=True,
+            text=True,
+        )
+        if result.returncode != 0:
+            print("Failed to run C program with benchmark")
+            print(result.stderr)
+            return False
+        print("C program and benchmark ran successfully\n")
+
+        print("=== Timing Results ===")
+        print(result.stderr)
+        print("======================\n")
+    else:
+        print(
+            f"Running prime finder with {processes} processes, {threads} threads, range {iterations}, start {start_num}..."
+        )
+        result = subprocess.run(
+            ["./a.out", str(processes), str(threads), str(iterations), str(start_num)],
+            capture_output=True,
+            text=True,
+        )
+        if result.returncode != 0:
+            print("Failed to run C program")
+            print(result.stderr)
+            return False
+        print("C program ran successfully...\n")
+    return True
+
+
 def clean_directory():
     print("Cleaning up directory...")
     result = subprocess.run(args=["make", "clean"], capture_output=True, text=True)
@@ -15,6 +66,81 @@ def clean_directory():
         return False
     print("Directory cleaned succesfully")
     return True
+
+
+def load_prime_data(processes, threads, iterations, start_num, benchmark_toggle=False):
+    if (
+        not clean_directory()
+        or not compile_c_program()
+        or not run_c_program(
+            processes, threads, iterations, start_num, benchmark_toggle
+        )
+    ):
+        return None
+
+    files = glob.glob("primes/*.csv")
+    if not files:
+        print("Failed to find any prime files")
+        return None
+
+    df = pd.concat(
+        [pd.read_csv(f, names=["process", "thread", "prime"]) for f in files]
+    ).sort_values("prime")
+    df.to_csv("all_primes.csv", index=False)
+    return df
+
+
+def plot_primes(primes, gaps, large_toggle):
+    fig, axes = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
+    if not large_toggle:
+        axes[0].scatter(
+            primes,
+            np.array([i for i in range(len(primes))]),
+            s=30,
+            c=primes,
+            cmap="twilight",
+            marker="o",
+            alpha=0.9,
+        )
+
+        axes[1].scatter(
+            primes[1:],
+            gaps,
+            s=30,
+            c=primes[1:],
+            cmap="twilight",
+            marker="o",
+            alpha=0.8,
+        )
+    else:
+        axes[0].scatter(
+            primes,
+            np.array([i for i in range(len(primes))]),
+            s=30,
+            c="purple",
+            marker=".",
+            alpha=0.7,
+        )
+        axes[1].set_yscale("log")
+        axes[1].scatter(
+            primes[1:],
+            gaps,
+            s=30,
+            c="purple",
+            marker=".",
+            alpha=0.3,
+        )
+
+    axes[0].set_ylabel("Primes")
+    axes[0].set_title(f"{len(primes)} Primes")
+    axes[0].grid(True, alpha=0.3)
+
+    axes[1].set_xlabel("Numbers")
+    axes[1].set_ylabel("Gap to Next Prime")
+    axes[1].grid(True, alpha=0.3)
+
+    plt.tight_layout()
+    plt.show()
 
 
 def check_all_args(argv):
@@ -100,130 +226,6 @@ def get_args(argv):
     return df, verbose, large_toggle, no_plot
 
 
-def compile_c_program():
-    print("Compiling C program...")
-    result = subprocess.run(["make", "parallel_build"], capture_output=True, text=True)
-    if result.returncode != 0:
-        print("Failed to compile C with make all")
-        return False
-    print("C program compliled succesfully...\n")
-    return True
-
-
-def run_c_program(processes, threads, iterations, start_num, benchmark_toggle=False):
-    if benchmark_toggle:
-        print(
-            f"Running benchmark on prime finder with {processes} processes, {threads} threads, range {iterations}, start {start_num}..."
-        )
-        result = subprocess.run(
-            [
-                "make",
-                "benchmark",
-                f"ARGS={processes} {threads} {iterations} {start_num}",
-            ],
-            cwd=os.getcwd(),
-            capture_output=True,
-            text=True,
-        )
-        if result.returncode != 0:
-            print("Failed to run C program with benchmark")
-            return False
-        print("C program and benchmark ran successfully\n")
-
-        print("=== Timing Results ===")
-        print(result.stderr)
-        print("======================\n")
-    else:
-        print(
-            f"Running prime finder with {processes} processes, {threads} threads, range {iterations}, start {start_num}..."
-        )
-        result = subprocess.run(
-            ["./a.out", str(processes), str(threads), str(iterations), str(start_num)],
-            capture_output=True,
-            text=True,
-        )
-        if result.returncode != 0:
-            print("Failed to run C program")
-            return False
-        print("C program ran successfully...\n")
-    return True
-
-
-def load_prime_data(processes, threads, iterations, start_num, benchmark_toggle=False):
-    if (
-        not clean_directory()
-        or not compile_c_program()
-        or not run_c_program(
-            processes, threads, iterations, start_num, benchmark_toggle
-        )
-    ):
-        return None
-
-    files = glob.glob("primes/*.csv")
-    if not files:
-        print("Failed to find any prime files")
-        return None
-
-    df = pd.concat(
-        [pd.read_csv(f, names=["process", "thread", "prime"]) for f in files]
-    ).sort_values("prime")
-    df.to_csv("all_primes.csv", index=False)
-    return df
-
-
-def plot_primes(primes, gaps, large_toggle):
-    fig, axes = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
-    if not large_toggle:
-        axes[0].scatter(
-            primes,
-            np.array([i for i in range(len(primes))]),
-            s=30,
-            c=primes,
-            cmap="twilight",
-            marker="o",
-            alpha=0.9,
-        )
-
-        axes[1].scatter(
-            primes[1:],
-            gaps,
-            s=30,
-            c=primes[1:],
-            cmap="twilight",
-            marker="o",
-            alpha=0.8,
-        )
-    else:
-        axes[0].scatter(
-            primes,
-            np.array([i for i in range(len(primes))]),
-            s=30,
-            c="purple",
-            marker=".",
-            alpha=0.7,
-        )
-        axes[1].set_yscale("log")
-        axes[1].scatter(
-            primes[1:],
-            gaps,
-            s=30,
-            c="purple",
-            marker=".",
-            alpha=0.3,
-        )
-
-    axes[0].set_ylabel("Primes")
-    axes[0].set_title(f"{len(primes)} Primes")
-    axes[0].grid(True, alpha=0.3)
-
-    axes[1].set_xlabel("Numbers")
-    axes[1].set_ylabel("Gap to Next Prime")
-    axes[1].grid(True, alpha=0.3)
-
-    plt.tight_layout()
-    plt.show()
-
-
 def main():
     flags = get_flags(sys.argv.copy())
     if flags is None:
@@ -256,7 +258,7 @@ def main():
         print(f"Largest gap between primes found {max(gaps)}")
         print(f"Average prime gap {np.average(gaps):.3f}")
         print(
-            f"Standard Deviation {np.sqrt(np.average(gaps ** 2) / np.average(gaps) ** 2):.3f}"
+            f"Standard deviation {np.sqrt(np.average(gaps ** 2) / np.average(gaps) ** 2):.3f}"
         )
 
         if verbose:
@@ -270,11 +272,10 @@ def main():
             )
         if not no_plot:
             plot_primes(primes, gaps, large_toggle)
+            print("\nProgram completed successfully")
     else:
-        print("Failed to find prime data")
-        return
-    print("\nProgram completed")
-
+        print("Program failed")
+    
 
 if __name__ == "__main__":
     main()

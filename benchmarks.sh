55c89cc78489dfecc564216b77d24291d7432923
Austin <ajb755@gmail.com>
2026-05-07 02:44:44 -0600

    Small fix

--- benchmarks.sh
+++ benchmarks.sh
@@ -2,15 +2,15 @@
 
 exec > benchmarks.txt 2>&1
 
-procs=(1 2 4 8 16)
-thrds=(1 2 4 8 16)
-for i in {0..4}; do
-    for j in {0..4}; do
-        echo BENCHMARK RUN $(((i*5+j)+1))
+procs=(1 2 4 8)
+thrds=(1 2 4 8)
+for i in {0..3}; do
+    for j in {0..3}; do
+        echo BENCHMARK RUN $(((i*4+j)+1))
         echo ==============================================
         echo ==============================================
         echo
-        for k in {0..10}; do
+        for k in {0..5}; do
             echo =====${k}=====
             python plot_primes.py -npb ${procs[i]} ${thrds[j]} $((10000000/(procs[i]*thrds[j]))) $((RANDOM * 100000))
             echo ==============
